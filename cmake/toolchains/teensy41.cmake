da29ed59b7db44972c06ba493ee3a0c0df486270
Newdigate <nicnewdigate@gmail.com>
2022-07-02 16:40:15 +0100

    corrections to ARM compiler path for CI

--- cmake/toolchains/teensy41.cmake
+++ cmake/toolchains/teensy41.cmake
@@ -2,10 +2,10 @@ set(TEENSY_VERSION 41 CACHE STRING "Set to the Teensy version corresponding to y
 set(CPU_CORE_SPEED 600000000 CACHE STRING "Set to 24000000, 48000000, 72000000 or 96000000 to set CPU core speed" FORCE) # Derived variables
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nano.specs" CACHE INTERNAL "")
 #teensy compiler options
-#set(COMPILERPATH "/opt/gcc-arm-none-eabi-9-2019-q4-major/bin/")
-set(COMPILERPATH "/Applications/ARM_9_3/bin/")
-#set(DEPSPATH "/home/runner/work/teensy-gfx/teensy-audio-launch-ctrl/deps")
-set(DEPSPATH "/Users/nicholasnewdigate/Development/github/newdigate/temp_dep")
+set(COMPILERPATH "/opt/gcc-arm-none-eabi-9-2019-q4-major/bin/")
+#set(COMPILERPATH "/Applications/ARM_9_3/bin/")
+set(DEPSPATH "/home/runner/work/teensy-gfx/teensy-gfx/deps")
+#set(DEPSPATH "/Users/nicholasnewdigate/Development/github/newdigate/temp_dep")
 set(COREPATH "${DEPSPATH}/cores/teensy4/")
 set(BUILD_FOR_TEENSY ON)
 add_definitions(-DTEENSY_VERSION=${TEENSY_VERSION})
