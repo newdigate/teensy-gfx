02890f9ee5a482e9e549cbc7c9827eb202f6e61b
Newdigate <nicnewdigate@gmail.com>
2022-07-03 22:16:48 +0100

    using dockerhub container container nicnewdigate/docker-teensy-cmake-macros:0.5

--- cmake/toolchains/teensy41.cmake
+++ cmake/toolchains/teensy41.cmake
@@ -2,7 +2,7 @@ set(TEENSY_VERSION 41 CACHE STRING "Set to the Teensy version corresponding to y
 set(CPU_CORE_SPEED 600000000 CACHE STRING "Set to 24000000, 48000000, 72000000 or 96000000 to set CPU core speed" FORCE) # Derived variables
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nano.specs" CACHE INTERNAL "")
 #teensy compiler options
-set(COMPILERPATH "/__w/gcc-arm-none-eabi-10.3-2021.10/bin/")
+set(COMPILERPATH "/home/runner/work/gcc-arm-none-eabi-10.3-2021.10/bin/")
 #set(COMPILERPATH "/Applications/ARM_9_3/bin/")
 set(DEPSPATH "/__w/deps")
 #set(DEPSPATH "/Users/nicholasnewdigate/Development/github/newdigate/temp_dep")
