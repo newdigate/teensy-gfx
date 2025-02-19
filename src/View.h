304e879b289d9ecc7148c83fa755260825f03f08
newdigate <nicnewdigate@gmail.com>
2025-02-19 17:34:35 +0000

    move GFXglyph to gfxfont.h

--- src/View.h
+++ src/View.h
@@ -27,28 +27,10 @@
 #endif
 
 #ifdef BUILD_FOR_LINUX // these are defined in adafruit_st77xx libraries for arduino
-
+#include "gfxfont.h"
 // Map fonts that were modified back to the ILI9341 font
 #define ST7735_t3_font_t ILI9341_t3_font_t
 
-/// Font data stored PER GLYPH
-typedef struct {
-    uint16_t bitmapOffset;     ///< Pointer into GFXfont->bitmap
-    uint8_t  width;            ///< Bitmap dimensions in pixels
-    uint8_t  height;           ///< Bitmap dimensions in pixels
-    uint8_t  xAdvance;         ///< Distance to advance cursor (x axis)
-    int8_t   xOffset;          ///< X dist from cursor pos to UL corner
-    int8_t   yOffset;          ///< Y dist from cursor pos to UL corner
-} GFXglyph;
-
-/// Data stored for FONT AS A WHOLE
-typedef struct {
-    uint8_t  *bitmap;      ///< Glyph bitmaps, concatenated
-    GFXglyph *glyph;       ///< Glyph array
-    uint8_t   first;       ///< ASCII extents (first char)
-    uint8_t   last;        ///< ASCII extents (last char)
-    uint8_t   yAdvance;    ///< Newline distance (y axis)
-} GFXfont;
 #endif
 
 #ifndef tgfx_swap
